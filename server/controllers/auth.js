0eca7771747979a9eb8c60b8ad621669c625fa99
jon-zawada <jonathan_zawada@yahoo.com>
2023-08-23 12:38:38 -1000

    prettify/lint

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -45,16 +45,16 @@ export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email: email });
-    if(!user) {
-      return res.status(400).json({ msg: "User does not exist."});
+    if (!user) {
+      return res.status(400).json({ msg: "User does not exist." });
     } else {
       const isMatch = await bcrypt.compare(password, user.password);
-      if(!isMatch) {
-        return res.status(400).json({ msg: "Invalid credentials."});
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid credentials." });
       } else {
-        const token = jwt.sign({id: user._id, }, process.env.JWT_SECRET)
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
         delete user.password;
-        res.status(201).json({token, user})
+        res.status(201).json({ token, user });
       }
     }
   } catch (err) {
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -36,7 +36,7 @@ export const addRemoveFriend = async (req, res) => {
     const user = await User.findById(id);
     const friend = await User.findById(friendId);
 
-    if(user.friends.includes(friendId)) {
+    if (user.friends.includes(friendId)) {
       user.friends = user.friends.filter((id) => id !== friendId);
       friend.friends = friend.friends.filter((userId) => userId !== id);
     } else {
@@ -58,4 +58,4 @@ export const addRemoveFriend = async (req, res) => {
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
-}
+};
