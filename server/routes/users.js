2f4d689a0f31a6f13a05d90504aeffc5b4c724cf
jon-zawada <jonathan_zawada@yahoo.com>
2023-08-23 00:48:45 -1000

    adds user get and friend addremove

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,8 +2,8 @@ import express from "express";
 
 import {
   getUser,
-  // getUserFriends,
-  // addRemoveFriend,
+  getUserFriends,
+  addRemoveFriend,
 } from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
 
@@ -11,9 +11,9 @@ const router = express.Router();
 
 /* READ */
 router.get("/:id", verifyToken, getUser);
-// router.get("/:id/friends", verifyToken, getUserFriends);
+router.get("/:id/friends", verifyToken, getUserFriends);
 
 /* UPDATE */
-// router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
 export default router;
