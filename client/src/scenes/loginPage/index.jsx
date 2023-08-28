97b159f3d5578debc3de09dceedc6e140841a021
jon-zawada <jonathan_zawada@yahoo.com>
2023-08-28 03:48:20 -0700

    login and register page/form

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,7 +1,40 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
+
 const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
   return (
-    <div>Login Page</div>
-  )
-}
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Socialpedia
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+        // textAlign="center"
+      >
+        <Typography
+          fontWeight="500"
+          variant="h5"
+          sx={{ mb: "1.5rem" }}
+        >
+          Welcome to SocialPedia the social media for sociopaths!
+        </Typography>
+        <Form/>
+      </Box>
+    </Box>
+  );
+};
 
 export default LoginPage;
